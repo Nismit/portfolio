9e000c3a1dd04cf15632b4612c936ec9cbfdf00d
git <nismit.dev@gmail.com>
2019-03-19 22:06:53 -0700

    Modify header component styling

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,31 @@
 import Link from 'next/link';
 
 const Header = () => (
-  <header>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>Works</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
+  <header className="header">
+    <div className="header__container">
+      <Link href="/">
+        <a style={linkStyle}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={linkStyle}>Works</a>
+      </Link>
+      <Link href="/about">
+        <a style={linkStyle}>About</a>
+      </Link>
+    </div>
+    <style jsx>{`
+      .header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 10;
+      }
+
+      .header__container {
+        width: 100%;
+        padding: 2rem;
+      }
+    `}</style>
   </header>
 )
 
