4b535a2c9ef26e207e8a6091853c4c3a96a020ec
git <nismit.dev@gmail.com>
2019-03-20 23:11:06 -0700

    Add base styling and font-face

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -2,7 +2,7 @@ import Header from '../components/Header';
 import MainVisual from '../components/MainVisual';
 
 const MainLayout = props => (
-  <div>
+  <div className="wrapper">
     <Header />
     <div>
       <div className="main__container">
@@ -26,7 +26,38 @@ const MainLayout = props => (
       body {
         margin: 0;
         color: #fff;
+        font-size: 16px;
         background-color #000;
+      }
+
+      @font-face {
+        font-family: 'DIN condensed';
+        src:  local('DIN condensed'),
+              url('/static/fonts/din-condensed-bold.woff2') format('woff2'),
+              url('/static/fonts/din-condensed-bold.woff')  format('woff'),
+              url('/static/fonts/din-condensed-bold.ttf')   format('truetype'),
+              url('/static/fonts/din-condensed-bold.eot')   format('embedded-opentype');
+        font-weight: 700;
+        font-style: normal;
+      }
+
+      h1, h2, h3, h4 {
+        font-family: 'DIN condensed';
+        letter-spacing: 2px;
+        text-transform: uppercase;
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+
+      h1 {
+        letter-spacing: 7px;
+        font-size: 7rem;
+        line-height: 0.95;
+      }
+
+      .wrapper {
+        padding-left: 1.5rem;
+        padding-right: 1.5rem;
       }
 
       .main__container {
