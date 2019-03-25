e5b3282848e2f0d07b7222f5ed28e25513455904
git <nismit.dev@gmail.com>
2019-03-24 18:11:59 -0700

    Implement scroll sync with main visual

---
+++ components/ComponentHeadBlock.js
@@ -0,0 +1,17 @@
+
+export default () => (
+  <div className="head-block">
+    <div>
+      <h1>About Me</h1>
+      <p>Tests</p>
+    </div>
+
+    <style jsx>{`
+      .head-block {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+      }
+    `}</style>
+  </div>
+)
--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,54 @@
+import React, { Component } from 'react';
 import SmoothScrollbar from 'smooth-scrollbar';
 import Scrollbar from 'react-smooth-scrollbar';
+import ComponentHeadBlock from '../components/ComponentHeadBlock';
 import ComponentTextBlock from '../components/ComponentTextBlock';
 
-const aboutHead = {
-  height: '100vh'
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.containerRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.containerRef.current.scrollbar.addListener(() => this.onUpdateScroll());
+    const visualContainer = document.querySelector('.global-visual');
+    this.visualContainer = visualContainer;
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  onUpdateScroll() {
+    const { scrollbar } = this.containerRef.current;
+    this.visualContainer.style.transform = `translate3d(0,-${scrollbar.offset.y}px, 0)`;
+  }
+
+  render() {
+    return (
+      <Scrollbar ref={this.containerRef} thumbMinSize={10} className="page about virtual-scroll">
+        <div className="about__header">
+          <ComponentHeadBlock />
+        </div>
+
+        <div className="about__content">
+
+          <ComponentTextBlock />
+          <ComponentTextBlock />
+          <ComponentTextBlock />
+
+        </div>
+        <style jsx>{`
+          .about__head {
+            position: fixed;
+            width: 100%;
+            height: 100vh:
+          }
+        `}</style>
+      </Scrollbar>
+    )
+  }
 }
 
-export default () => (
-  <Scrollbar className="page about virtual-scroll">
-    <div className="about__header" style={aboutHead}>
-      <p>Header</p>
-    </div>
-
-    <div className="about__content">
-
-      <ComponentTextBlock />
-
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-    </div>
-    <style jsx>{`
-      .about__head {
-        position: fixed;
-        width: 100%;
-        height: 100vh:
-      }
-    `}</style>
-  </Scrollbar>
-)
+export default About
