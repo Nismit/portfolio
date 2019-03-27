0317d580233b58c47f9e88af83728cabb279e05c
git <nismit.dev@gmail.com>
2019-03-26 23:59:54 -0700

    Modify header styling

--- pages/about.js
+++ pages/about.js
@@ -68,10 +68,11 @@ export default class About extends PureComponent {
 
           </div>
           <style jsx>{`
-            .about__head {
-              position: fixed;
+            .about__header {
               width: 100%;
               height: 100vh:
+              background: -webkit-linear-gradient(top, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 100%);
+              background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 100%);
             }
           `}</style>
         </Scrollbar>
