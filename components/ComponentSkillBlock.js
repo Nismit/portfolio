a0867581642b3898214890ac905010ad461784fd
git <nismit.dev@gmail.com>
2019-03-27 00:00:10 -0700

    Add some styling into skill component

--- components/ComponentSkillBlock.js
+++ components/ComponentSkillBlock.js
@@ -14,13 +14,16 @@ export default (props) => {
         skillGroup.map((item) => (
           <div key={item.sys.id}>
             <h3>{item.fields.title}</h3>
-            {
-              item.fields.skills.map((skill) =>
-                <div key={skill.sys.id} data-level={skill.fields.level}>
-                  <p>{skill.fields.title}</p>
-                </div>
-              )
-            }
+
+            <div className="skills">
+              {
+                item.fields.skills.map((skill) =>
+                  <div className="skill" key={skill.sys.id} data-level={skill.fields.level}>
+                    <span>{skill.fields.title}</span>
+                  </div>
+                )
+              }
+            </div>
           </div>
         ))
       }
@@ -31,6 +34,69 @@ export default (props) => {
           padding-bottom: 4rem;
           padding-left: 1rem;
           padding-right: 1rem;
+        }
+
+        h3 {
+          margin-top: 1.5rem;
+        }
+
+        .skills {
+          display: flex;
+        }
+
+        .skill {
+          padding: 3px;
+          margin-right: 1rem;
+          margin-bottom: 1rem;
+        }
+
+        .skill > span {
+          display: inline-block;
+          padding: 1rem 1.5rem;
+          background-color: #000;
+        }
+
+        .skill[data-level="3"] {
+          background: linear-gradient(270deg, #26ecee, #0283cc);
+          background-size: 200% 200%;
+
+          -webkit-animation: AnimationName 5s ease infinite;
+          -moz-animation: AnimationName 5s ease infinite;
+          animation: AnimationName 5s ease infinite;
+        }
+
+        .skill[data-level="2"] {
+          background: linear-gradient(270deg, #6c9fe6, #906dca);
+          background-size: 200% 200%;
+
+          -webkit-animation: AnimationName 5s ease infinite;
+          -moz-animation: AnimationName 5s ease infinite;
+          animation: AnimationName 5s ease infinite;
+        }
+
+        .skill[data-level="1"] {
+          background: linear-gradient(270deg, #d03332, #ca8f6d);
+          background-size: 200% 200%;
+
+          -webkit-animation: AnimationName 5s ease infinite;
+          -moz-animation: AnimationName 5s ease infinite;
+          animation: AnimationName 5s ease infinite;
+        }
+
+        @-webkit-keyframes AnimationName {
+          0%{background-position:0% 50%}
+          50%{background-position:100% 50%}
+          100%{background-position:0% 50%}
+        }
+        @-moz-keyframes AnimationName {
+            0%{background-position:0% 50%}
+            50%{background-position:100% 50%}
+            100%{background-position:0% 50%}
+        }
+        @keyframes AnimationName { 
+            0%{background-position:0% 50%}
+            50%{background-position:100% 50%}
+            100%{background-position:0% 50%}
         }
       `}</style>
     </div>
