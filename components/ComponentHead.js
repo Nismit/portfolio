ec9047d8975edf64c79c543fae46760792bdf750
git <nismit.dev@gmail.com>
2019-03-28 17:53:16 -0700

    Modify selector

--- components/ComponentHead.js
+++ components/ComponentHead.js
@@ -8,7 +8,7 @@ export default (props) => {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"></link>
-        <style>{`p { font-family: 'Open Sans', sans-serif; }`}</style>
+        <style>{`body { font-family: 'Open Sans', sans-serif; }`}</style>
       </Head>
     </div>
   );
