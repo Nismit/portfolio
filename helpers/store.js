52ef667204ebd462c9963f0c6f8236f2b2a993a9
git <nismit.dev@gmail.com>
2019-04-01 23:32:35 -0700

    WIP making a store

---
+++ helpers/store.js
@@ -0,0 +1,52 @@
+import { createStore } from 'redux';
+
+const defaultState = {
+  isLoaded: false,
+  error: ''
+}
+
+// ACTION TYPES
+export const actionTypes = {
+  THREE_LOADED: {
+    ERROR: 'THREE_LOADED_ERROR',
+    SUCCESS: 'THREE_LOADED_SUCCESS',
+    PENDING: 'THREE_LOADED_PENDING'
+  },
+  THREE_ANIMATED: {
+    ERROR: 'THREE_ANIMATED_ERROR',
+    SUCCESS: 'THREE_ANIMATED_SUCCESS',
+    PENDING: 'THREE_ANIMATED_PENDING'
+  },
+}
+
+// REDUCERS
+export const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case actionTypes.THREE_LOADED.ERROR:
+      return {
+        ...state,
+        error: action.error
+      };
+    case actionTypes.THREE_LOADED.SUCCESS:
+      return {
+        ...state,
+        isLoaded: true
+      };
+    case actionTypes.THREE_LOADED.PENDING:
+      return {
+        ...state,
+        isLoaded: false
+      };
+    default:
+      return state;
+  }
+}
+
+// ACTIONS
+export const serverRenderClock = () => {
+  return { type: actionTypes.TICK, light: false, ts: Date.now() }
+}
+
+export function initializeStore(initialState = defaultState) {
+  return createStore(reducer, initialState)
+}
