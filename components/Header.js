cae9fb43976170357adce75a0514395173031a9f
git <nismit.dev@gmail.com>
2019-04-13 17:51:17 -0700

    Disable auto scroll top function

--- components/Header.js
+++ components/Header.js
@@ -4,17 +4,17 @@ const Header = () => (
   <header className="header">
     <div className="header__container">
       <div>
-        <Link href="/">
+        <Link scroll={false} href="/">
           <a className="logo">
             <svg xmlns="http://www.w3.org/2000/svg" width="83.061" height="27.143">
               <path fill="#FFF" d="M74.678 1.295v15.16L56.612 1.295H45.245l29.426 24.692.007-.008v.008h7.307V1.295zM55.215 25.987h11.367L37.172 1.309l-.012-.014-.002.002-.003-.002-.012.014-17.992 15.098L1.146 1.298v9.538l18.006 15.11.007-.008 17.998-15.103z" />
             </svg>
           </a>
         </Link>
-        <Link href="/projects">
+        <Link scroll={false} href="/projects">
           <a className="menu__link">Projects</a>
         </Link>
-        <Link href="/about">
+        <Link scroll={false} href="/about">
           <a className="menu__link">About</a>
         </Link>
       </div>
