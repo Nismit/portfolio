e9a9d0d2169932a670f402a75e51478ec51324ac
Nismit <nismit.dev@gmail.com>
2020-08-08 00:59:24 -0700

    Rename Document from CustomDocument class name

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,11 @@
 // _document is only rendered on the server side and not on the client side
 // Event handlers like onClick can't be added to this file
 
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class CustomDocument extends Document {
+export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
+    const initialProps = await NextDocument.getInitialProps(ctx);
     return { ...initialProps };
   }
 
