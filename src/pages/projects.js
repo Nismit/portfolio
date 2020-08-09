5362e3304aa2c37ff4e3ebec8ad31ad20b1a1dcb
Nismit <nismit.dev@gmail.com>
2020-08-09 15:47:18 -0700

    Re-formatted indent and add styled components

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,52 +4,44 @@ import ComponentHead from '../components/ComponentHead';
 import ComponentProjectHeadBlock from '../components/ComponentProjectHeadBlock';
 
 function Projects() {
-  const [projectNumber, setProjectNumber] = useState(0);
+    const [projectNumber, setProjectNumber] = useState(0);
 
-  const navToNextProject = () => {
-    const limit = data.allProjects.length - 1;
-    const newProjectNumber = projectNumber + 1;
+    const navToNextProject = () => {
+        const limit = data.allProjects.length - 1;
+        const newProjectNumber = projectNumber + 1;
 
-    if(newProjectNumber > limit) {
-      setProjectNumber(0);
-    } else {
-      setProjectNumber(newProjectNumber);
+        if (newProjectNumber > limit) {
+            setProjectNumber(0);
+        } else {
+            setProjectNumber(newProjectNumber);
+        }
     }
-  }
 
-  const navToPrevProject = () => {
-    const limit = data.allProjects.length - 1;
-    const newProjectNumber = projectNumber - 1;
+    const navToPrevProject = () => {
+        const limit = data.allProjects.length - 1;
+        const newProjectNumber = projectNumber - 1;
 
-    if(newProjectNumber < 0) {
-      setProjectNumber(limit);
-    } else {
-      setProjectNumber(newProjectNumber);
+        if (newProjectNumber < 0) {
+            setProjectNumber(limit);
+        } else {
+            setProjectNumber(newProjectNumber);
+        }
     }
-  }
 
-  return (
-    <>
-      <ComponentHead headTitle="Project" />
+    return (
+        <>
+            <ComponentHead headTitle="Project" />
 
-      <div className="project__header">
-        <ComponentProjectHeadBlock
-          projectId={projectNumber}
-          title={data.allProjects[projectNumber].fields.title}
-          subTitle={data.allProjects[projectNumber].fields.subTitle}
-          navigateNext={() => { navToNextProject() }}
-          navigatePrev={() => { navToPrevProject() }}
-        />
-      </div>
-
-      <style jsx>{`
-          .project__header {
-            width: 100%;
-            height: 100vh;
-            position: relative;
-          }
-        `}</style>
-    </>
-  )
+            <div className="project__header">
+                <ComponentProjectHeadBlock
+                    projectId={projectNumber}
+                    title={data.allProjects[projectNumber].fields.title}
+                    subTitle={data.allProjects[projectNumber].fields.subTitle}
+                    navigateNext={() => { navToNextProject() }}
+                    navigatePrev={() => { navToPrevProject() }}
+                />
+            </div>
+        </>
+    )
 }
 export default Projects;
