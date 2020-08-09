070e544bb3b3dbbca561505991a08580a046cab6
Nismit <nismit.dev@gmail.com>
2020-08-08 23:49:24 -0700

    Refactor to functional component

--- src/components/MainVisual.js
+++ src/components/MainVisual.js
@@ -1,55 +1,30 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { useRef, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import * as THREE from 'three';
 import Terrain from '../helpers/Terrain';
 import Loading from '../components/Loading';
-import { threeStart, threeLoaded } from '../helpers/store';
+import { actionTypes } from '../helpers/store';
 
-const mapStateToProps = state => ({
-  localIsLoaded: state.isLoaded
-});
+const MainVisual = () => {
+  let scene, camera, renderer, obj, clock, frameId;
+  const refContainer = useRef(null);
+  const dispatch = useDispatch();
+  const _isLoaded = useSelector(state => state.isLoaded);
+  const [resolution, setResolution] = useState({width: 0, height: 0});
 
-const mapDispatchToProps = dispatch => ({
-  initThreeByDispatch: () => {
-    dispatch(threeStart())
-  },
-  loadedThreeByDispatch: () => {
-    dispatch(threeLoaded())
-  }
-});
+  useEffect(() => {
+    dispatch({ type: actionTypes.THREE_LOADED.PENDING });
+    const width = refContainer.current.clientWidth;
+    const height = refContainer.current.clientHeight;
 
-class MainVisual extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      resolution: {
-        width: 0,
-        height: 0
-      },
-      isLoaded: false,
-      progress: 0
-    }
-
-    this.loop = this.loop.bind(this);
-    this.stop = this.stop.bind(this);
-    this.draw = this.draw.bind(this);
-    this.onUpdateScreen = this.onUpdateScreen.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.initThreeByDispatch();
-    const width = this.mount.clientWidth;
-    const height = this.mount.clientHeight;
-
-    const scene = new THREE.Scene();
-    const clock = new THREE.Clock({ autoStart: false });
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    const camera = new THREE.PerspectiveCamera(60, width / height, .1, 10000);
+    scene = new THREE.Scene();
+    clock = new THREE.Clock({ autoStart: false });
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    camera = new THREE.PerspectiveCamera(60, width / height, .1, 10000);
     camera.position.y = 13;
     camera.position.z = 4;
 
-    const obj = new Terrain();
+    obj = new Terrain();
 
     const isLoaded = obj.textureLoad();
     if (isLoaded !== null) {
@@ -60,77 +35,67 @@ class MainVisual extends Component {
     renderer.setClearColor('#000000');
     renderer.setSize(width, height);
 
-    this.scene = scene;
-    this.camera = camera;
-    this.renderer = renderer;
-    this.obj = obj;
-    this.clock = clock;
+    onUpdateScreen();
+    window.addEventListener('resize', onUpdateScreen);
 
-    this.onUpdateScreen();
-    window.addEventListener('resize', this.onUpdateScreen);
+    refContainer.current.appendChild(renderer.domElement);
 
-    this.mount.appendChild(this.renderer.domElement);
+    dispatch({ type: actionTypes.THREE_LOADED.SUCCESS });
+    clock.start();
+    loop();
 
-    this.props.loadedThreeByDispatch();
-    this.clock.start();
-    this.loop();
+    return () => {
+      stop();
+      refContainer.current.removeChild(renderer.domElement);
+      window.removeEventListener('resize', onUpdateScreen);
+    }
+  }, []);
+
+  const onUpdateScreen = () => {
+    const newResolution = {
+      width: document.documentElement.clientWidth,
+      height: window.innerHeight
+    }
+
+    setResolution({...newResolution});
+    const { width, height } = newResolution;
+
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
   }
 
-  componentWillUnmount() {
-    this.stop();
-    this.mount.removeChild(this.renderer.domElement);
-    window.removeEventListener('resize', this.onUpdateScreen);
+  const stop = () => {
+    clock.stop();
+    cancelAnimationFrame(frameId);
   }
 
-  onUpdateScreen() {
-    this.setState(prevState => ({
-      resolution: {
-        ...prevState.resolution,
-        width: document.documentElement.clientWidth,
-        height: window.innerHeight
-      }
-    }), () => {
-      const { width, height } = this.state.resolution;
-
-      this.renderer.setSize(width, height);
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-    });
-  }
-
-  stop() {
-    this.clock.stop();
-    cancelAnimationFrame(this.frameId);
-  }
-
-  loop() {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.draw);
+  const loop = () => {
+    if (!frameId) {
+      frameId = requestAnimationFrame(draw);
     }
   }
 
-  draw() {
-    const time = this.clock.getDelta();
-    this.obj.render(time);
+  const draw = () => {
+    const time = clock.getDelta();
+    obj.render(time);
 
-    this.renderer.render(this.scene, this.camera);
-    this.frameId = window.requestAnimationFrame(this.draw);
+    renderer.render(scene, camera);
+    frameId = window.requestAnimationFrame(draw);
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Loading isLoaded={this.props.localIsLoaded} />
-        <div
-          className="global-visual"
-          style={{ width: '100vw', height: '100%', position: 'fixed', top: '0', left: '0', zIndex: '1' }}
-          ref={(mount) => { this.mount = mount }}
-        />
-      </React.Fragment>
-    )
-  }
+  return (
+    <>
+      <Loading isLoaded={_isLoaded} />
+      <div
+        className="global-visual"
+        style={{ width: '100vw', height: '100%', position: 'fixed', top: '0', left: '0', zIndex: '1' }}
+        ref={refContainer}
+      />
+    </>
+  )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainVisual);
+export default MainVisual;
