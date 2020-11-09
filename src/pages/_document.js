0cbf439323900753cfaddc94331721e122b86b3a
Nismit <nismit.dev@gmail.com>
2020-11-08 23:04:59 -0800

    Fix favicon path

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,7 +14,7 @@ export default class Document extends NextDocument {
             <Html lang="en">
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+                    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"></link>
                 </Head>
                 <body>
