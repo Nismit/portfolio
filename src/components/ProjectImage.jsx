e4cdaa5c3c0afc9a2175b95fe2bf1478fb2e6998
Nismit <nismit.dev@gmail.com>
2020-11-21 23:14:04 -0800

    Add video option

--- src/components/ProjectImage.jsx
+++ src/components/ProjectImage.jsx
@@ -1,23 +1,31 @@
 function ProjectImage({ props }) {
     let fallback;
 
-    return (
-        <picture className="project__image">
-            {
-                props.map((content, i) => {
-                    if(content.fields.file.contentType === 'image/png') {
-                        fallback = content.fields.file.url;
-                        return false;
-                    } else {
-                        return <source key={i} srcSet={content.fields.file.url} type={content.fields.file.contentType} />
-                    }
-                })
-            }
-            {
-                <img src={fallback} alt="Hero Image" />
-            }
-        </picture>
-    )
+    if(props.length > 1) {
+        return (
+            <picture className="project__image">
+                {
+                    props.map((content, i) => {
+                        if(content.fields.file.contentType === 'image/png') {
+                            fallback = content.fields.file.url;
+                            return false;
+                        } else {
+                            return <source key={i} srcSet={content.fields.file.url} type={content.fields.file.contentType} />
+                        }
+                    })
+                }
+                {
+                    <img src={fallback} alt="Hero Image" />
+                }
+            </picture>
+        )
+    } else {
+        return (
+            <video loop autoPlay muted className="project__video">
+                <source src={props[0].fields.file.url} />
+            </video>
+        )
+    }
 }
 
 export default ProjectImage;
