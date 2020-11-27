51e1709ba6240d920bfcf2fad0af048bdcdf8e10
Nismit <nismit.dev@gmail.com>
2020-11-26 21:13:27 -0800

    Add position values

--- src/shaders/vertexSmoke.js
+++ src/shaders/vertexSmoke.js
@@ -5,7 +5,7 @@ precision mediump float;
 // attribute vec3 position;
 
 void main() {
-    gl_Position = vec4( position, 1.0 );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
 `;
 
