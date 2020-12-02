0d898c7908587874e942e9ff0829e91b0917eaff
Nismit <nismit.dev@gmail.com>
2020-12-01 22:01:45 -0800

    Different fbm

--- src/shaders/fragmentSmoke.js
+++ src/shaders/fragmentSmoke.js
@@ -4,6 +4,8 @@ uniform float u_time;
 uniform float u_ratio;
 uniform vec2 u_resolution;
 
+const mat2 m = mat2( 0.80,  0.60, -0.60,  0.80 );
+
 vec3 hsv2rgb(vec3 c) {
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
@@ -12,41 +14,60 @@ vec3 hsv2rgb(vec3 c) {
 
 // Ref: https://thebookofshaders.com/13/
 
-float random (in vec2 _st) {
-    return fract(sin(dot(_st.xy, vec2(12.9898,78.233)))* 43758.5453123);
+// float random (in vec2 _st) {
+//     return fract(sin(dot(_st.xy, vec2(12.9898,78.233)))* 43758.5453123);
+// }
+
+float noise( in vec2 p )
+{
+	return sin(p.x)*sin(p.y);
 }
 
 // Based on Morgan McGuire @morgan3d
 // https://www.shadertoy.com/view/4dS3Wd
-float noise (in vec2 _st) {
-    vec2 i = floor(_st);
-    vec2 f = fract(_st);
+// float noise (in vec2 _st) {
+//     vec2 i = floor(_st);
+//     vec2 f = fract(_st);
 
-    // Four corners in 2D of a tile
-    float a = random(i);
-    float b = random(i + vec2(1.0, 0.0));
-    float c = random(i + vec2(0.0, 1.0));
-    float d = random(i + vec2(1.0, 1.0));
+//     // Four corners in 2D of a tile
+//     float a = random(i);
+//     float b = random(i + vec2(1.0, 0.0));
+//     float c = random(i + vec2(0.0, 1.0));
+//     float d = random(i + vec2(1.0, 1.0));
 
-    vec2 u = f * f * (3.0 - 2.0 * f);
+//     vec2 u = f * f * (3.0 - 2.0 * f);
 
-    return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
-}
+//     return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+// }
 
 #define NUM_OCTAVES 4
 
-float fbm ( in vec2 _st) {
-    float v = 0.0;
-    float a = 0.5;
-    vec2 shift = vec2(100.0);
-    // Rotate to reduce axial bias
-    mat2 rot = mat2(cos(0.5), sin(0.5),-sin(0.5), cos(0.50));
-    for (int i = 0; i < NUM_OCTAVES; ++i) {
-        v += a * noise(_st);
-        _st = rot * _st * 2.0 + shift;
-        a *= 0.5;
+// float fbm ( in vec2 _st) {
+//     float v = 0.0;
+//     float a = 0.5;
+//     vec2 shift = vec2(100.0);
+//     // Rotate to reduce axial bias
+//     mat2 rot = mat2(cos(0.5), sin(0.5),-sin(0.5), cos(0.50));
+//     for (int i = 0; i < NUM_OCTAVES; ++i) {
+//         v += a * noise(_st);
+//         _st = rot * _st * 2.0 + shift;
+//         a *= 0.5;
+//     }
+//     return v;
+// }
+
+float fbm( in vec2 x ) {
+    float G = exp2(-1.0);
+    // float f = 1.0;
+    float a = 1.0;
+    float t = 0.0;
+    for(int i = 0; i < NUM_OCTAVES; i++ ) {
+        t += a * noise(x);
+        x = m*x*2.01;
+        // f *= 2.0;
+        a *= G;
     }
-    return v;
+    return t;
 }
 
 void main() {
